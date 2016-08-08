f0039bfdcf86ae90582f7670d325bf21b816c59f
pinbar <pinbar@users.noreply.github.com>
2016-08-07 23:14:09 -0400

    bug fixes

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var jwt = require('jsonwebtoken');
+var jwtUtil = require('jsonwebtoken');
 var config = require('./config');
 
 var app = express();
+
+var jsonParse = bodyParser.json();
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+//for static resources
 app.use(express.static('static'));
 
 //load logger after static to ignore static logging
 app.use(morgan('dev'));
-
-var jsonParse = bodyParser.json();
-var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 app.get('/', function(request, response){
     response.send('Welcome to the Matrix!');
@@ -25,22 +27,17 @@ app.post('/authenticate', urlEncodedParser, function(request, response){
     if(!request.body.applicationName && !request.body.applicationPassword) {
         response.status(400).send('Bad request');
     } else {
-        apiCredentials = {
-            name: request.body.applicationName,
-            password: request.body.applicationPassword
-        }
-        //credentials can be obtained from a DB
-        if(apiCredentials.name != config.appName && apiCredentials.password != config.appPassword) {
-            return response.status(401).end('Wrong password');
+        if(request.body.applicationName != config.appName || request.body.applicationPassword != config.appPassword) {
+            return response.status(401).end('Wrong credentials');
         } else {
             var claim = {
-                app: apiCredentials.name,
+                app: request.body.applicationName,
                 role: 'all'
             }
-            var token = jwt.sign(claim, config.jwtSecret, {
+            var token = jwtUtil.sign(claim, config.jwtSecret, {
                 expiresIn: 60
             });
-            response.send('JWT token created. Send the token in the header to make secure calls, like so => \n Authorization: Bearer ' + token);
+            response.send('JWT: ' + token);
         }
     }
 });
@@ -51,10 +48,10 @@ apiRouter.use(function(request, response, next){
     if(token) {
         token = token.replace("Bearer ","");
         console.log("token: " + token);
-        jwt.verify(token, config.jwtSecret, function(error, decoded){
+        jwtUtil.verify(token, config.jwtSecret, function(error, decoded){
             if(error) {
                 console.log("jwt error: " + error);
-                response.status(401).send('Token not authenticated. Error Message: ' + error);
+                response.status(401).send('Invalid Token. Error Message: ' + error);
             } else {
                 request.decodedToken = decoded;
                 next();
@@ -72,14 +69,11 @@ apiRouter.get('/levrai', function(request, response){
     response.send('Merovingian\'s \"secure\" restaurant!');
 });
 
-var server = app.listen('8081', function(){
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('App listening at http://%s:%s', host, port);
-});
-
 //default fallthrough handler
 app.use(function (req, res) {
     res.status(404).send('Resource Not Found');
+});
+
+var server = app.listen('8081', function(){
+    console.log('App listening at http://%s:%s', server.address().address, server.address().port);
 });
