0ffd56365bc956e7428361c4d6caa6ebff242584
pinbar <pinbar@users.noreply.github.com>
2016-08-10 21:50:37 -0400

    renaming and formatting

--- authService.js
+++ authService.js
@@ -1,19 +1,19 @@
 module.exports = {
-	//ideally this will be a service/DB to do this
-	authenticate: function(name, password) {
-		if(name === 'neo') {
-			if(password === 'keanu') {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		if(name === 'morpheus') {
-			if(password === 'laurence') {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
+    //ideally this will be a service/DB to do this
+    authenticate: function(name, password) {
+        if(name === 'neo') {
+            if(password === 'keanu') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if(name === 'morpheus') {
+            if(password === 'laurence') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
